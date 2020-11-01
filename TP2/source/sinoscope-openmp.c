7d8fe978cb376656909c64429229a6ace0360641
Philippe Belias <ossimfrancois@yahoo.fr>
2020-11-01 03:46:09 -0500

    resolution de la dimunition du fps en cours
    Signed-off-by: Philippe Belias <ossimfrancois@yahoo.fr>

--- TP2/source/sinoscope-openmp.c
+++ TP2/source/sinoscope-openmp.c
@@ -19,36 +19,50 @@ int sinoscope_image_openmp(sinoscope_t* sinoscope) {
         goto fail_exit;
     }
 
-    #pragma omp parallel for schedule(static)
-    for (int i = 0; i < sinoscope->width; i++) { 
-        #pragma omp parallel for schedule(static)
-        for (int j = 0; j < sinoscope->height; j++) {
-            float px    = sinoscope->dx * j - 2 * M_PI;
-            float py    = sinoscope->dy * i - 2 * M_PI;
-            float value = 0;
+    int i;
+    int j;
+    int k;
+    float px;
+    float py;
+    float value;
+    int index;
+    pixel_t pixel;
 
-            #pragma omp parallel for simd schedule(static)
-            for (int k = 1; k <= sinoscope->taylor; k += 2) {
+ 
+    
+   
+    #pragma omp parallel for schedule(static) private(i,j,k,px,py,value,index,pixel)
+    for (i = 0; i < sinoscope->width; i++) {
+     
+       #pragma omp parallel for schedule(static) 
+        for (j = 0; j < sinoscope->height; j++) {
+            px    = sinoscope->dx * j - 2 * M_PI;
+            py    = sinoscope->dy * i - 2 * M_PI;
+            value = 0;
+ 
+            #pragma omp parallel for simd schedule(static) 
+            for (k = 1; k <= sinoscope->taylor; k += 2) {
                 value += sin(px * k * sinoscope->phase1 + sinoscope->time) / k;
                 value += cos(py * k * sinoscope->phase0) / k;
             }
             
+            #pragma omp atomic write
             value = (atan(value) - atan(-value)) / M_PI;
             value = (value + 1) * 100;
 
-            pixel_t pixel;
-            //#pragma omp ordered simd
+            
             color_value(&pixel, value, sinoscope->interval, sinoscope->interval_inverse);
 
-            int index = (i * 3) + (j * 3) * sinoscope->width;
 
+            #pragma omp atomic write
+            index = (i * 3) + (j * 3) * sinoscope->width;
             sinoscope->buffer[index + 0] = pixel.bytes[0];
             sinoscope->buffer[index + 1] = pixel.bytes[1];
-            sinoscope->buffer[index + 2] = pixel.bytes[2];
-                    
+            sinoscope->buffer[index + 2] = pixel.bytes[2]; 
+            }
+
         }
 
-    }
     return 0;
 
     fail_exit:
