01f3b6cc4177303bea5d20780ee12bad4efb8248
Philippe Belias <ossimfrancois@yahoo.fr>
2020-10-31 22:50:44 -0400

    implementation open mp en cours probleme de grains dans l'image
    Signed-off-by: Philippe Belias <ossimfrancois@yahoo.fr>

--- TP2/source/sinoscope-openmp.c
+++ TP2/source/sinoscope-openmp.c
@@ -14,5 +14,42 @@ int sinoscope_image_openmp(sinoscope_t* sinoscope) {
      * utilisant les pragmas OpenMP selon votre énoncé.
      */
 
-    return -1;
+    if (sinoscope == NULL) {
+        LOG_ERROR_NULL_PTR();
+        goto fail_exit;
+    }
+
+    for (int i = 0; i < sinoscope->width; i++) {
+        #pragma omp parallel for simd schedule(static)
+        for (int j = 0; j < sinoscope->height; j++) {
+            float px    = sinoscope->dx * j - 2 * M_PI;
+            float py    = sinoscope->dy * i - 2 * M_PI;
+            float value = 0;
+
+            for (int k = 1; k <= sinoscope->taylor; k += 2) {
+                #pragma omp atomic
+                value += sin(px * k * sinoscope->phase1 + sinoscope->time) / k;
+                value += cos(py * k * sinoscope->phase0) / k;
+            }
+
+            value = (atan(value) - atan(-value)) / M_PI;
+            value = (value + 1) * 100;
+
+            pixel_t pixel;
+            color_value(&pixel, value, sinoscope->interval, sinoscope->interval_inverse);
+
+            int index = (i * 3) + (j * 3) * sinoscope->width;
+
+            sinoscope->buffer[index + 0] = pixel.bytes[0];
+            sinoscope->buffer[index + 1] = pixel.bytes[1];
+            sinoscope->buffer[index + 2] = pixel.bytes[2];
+                    
+        }
+
+    }
+    return 0;
+
+    fail_exit:
+        return -1;
+    
 }
