04b4460bde6254b409abdab6c1c66af12f73455f
Philippe Belias <ossimfrancois@yahoo.fr>
2020-11-03 17:36:19 -0500

    diminution des grains de sable dans l'image et augmentation des fps en bonne voie
    Signed-off-by: Philippe Belias <ossimfrancois@yahoo.fr>

--- TP2/source/sinoscope-openmp.c
+++ TP2/source/sinoscope-openmp.c
@@ -19,19 +19,26 @@ int sinoscope_image_openmp(sinoscope_t* sinoscope) {
         goto fail_exit;
     }
 
-    #pragma omp parallel for schedule(static) collapse(2)
-    for (int i = 0; i < sinoscope->width; i++) { 
-        //#pragma omp parallel for schedule(static)
-        for (int j = 0; j < sinoscope->height; j++) {
+    int i;
+    int j;
+    int k;
+    float value = 0;
+    
+
+    #pragma omp parallel for schedule(static) private(i,j,k) collapse(2) reduction(+: value) 
+    for (i = 0; i < sinoscope->width; i++) { 
+        for (j = 0; j < sinoscope->height; j++) {
             float px    = sinoscope->dx * j - 2 * M_PI;
             float py    = sinoscope->dy * i - 2 * M_PI;
-            float value = 0;
-
-            for (int k = 1; k <= sinoscope->taylor; k += 2) {
+            value = 0;
+            
+            
+            for (k = 1; k <= sinoscope->taylor; k += 2) {
                 value += sin(px * k * sinoscope->phase1 + sinoscope->time) / k;
                 value += cos(py * k * sinoscope->phase0) / k;
             }
-            
+
+           
             value = (atan(value) - atan(-value)) / M_PI;
             value = (value + 1) * 100;
 
@@ -39,12 +46,9 @@ int sinoscope_image_openmp(sinoscope_t* sinoscope) {
             color_value(&pixel, value, sinoscope->interval, sinoscope->interval_inverse);
 
             int index = (i * 3) + (j * 3) * sinoscope->width;
-
-            #pragma omp atomic write
+            #pragma omp critical
             sinoscope->buffer[index + 0] = pixel.bytes[0];
-            #pragma omp atomic write
             sinoscope->buffer[index + 1] = pixel.bytes[1];
-            #pragma omp atomic write
             sinoscope->buffer[index + 2] = pixel.bytes[2];
 
         }
