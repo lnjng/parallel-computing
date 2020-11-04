e7a438f0abccfb4464c4cb15dda1baeb9a552170
Philippe Belias <ossimfrancois@yahoo.fr>
2020-11-03 19:13:48 -0500

    nombre de fps grand et constant(stable à 26) resolution du probleme de grain dans l'image en cours
    Signed-off-by: Philippe Belias <ossimfrancois@yahoo.fr>

--- TP2/source/sinoscope-openmp.c
+++ TP2/source/sinoscope-openmp.c
@@ -22,36 +22,38 @@ int sinoscope_image_openmp(sinoscope_t* sinoscope) {
     int i;
     int j;
     int k;
-    float value = 0;
+    pixel_t pixel;
+    int index;
+   
     
 
-    #pragma omp parallel for schedule(static) private(i,j,k) collapse(2) reduction(+: value) 
+    #pragma omp parallel for schedule(static,10) private(i,j,k,pixel,index) collapse(2)
     for (i = 0; i < sinoscope->width; i++) { 
         for (j = 0; j < sinoscope->height; j++) {
             float px    = sinoscope->dx * j - 2 * M_PI;
             float py    = sinoscope->dy * i - 2 * M_PI;
-            value = 0;
+            float value = 0.0;
             
-            
+            #pragma omp simd reduction(+: value) 
             for (k = 1; k <= sinoscope->taylor; k += 2) {
                 value += sin(px * k * sinoscope->phase1 + sinoscope->time) / k;
                 value += cos(py * k * sinoscope->phase0) / k;
             }
 
-           
+
             value = (atan(value) - atan(-value)) / M_PI;
             value = (value + 1) * 100;
-
-            pixel_t pixel;
+            
             color_value(&pixel, value, sinoscope->interval, sinoscope->interval_inverse);
+            index = (i * 3) + (j * 3) * sinoscope->width;
 
-            int index = (i * 3) + (j * 3) * sinoscope->width;
-            #pragma omp critical (test)
+            #pragma omp critical(section1)
             {
                 sinoscope->buffer[index + 0] = pixel.bytes[0];
                 sinoscope->buffer[index + 1] = pixel.bytes[1];
                 sinoscope->buffer[index + 2] = pixel.bytes[2];
             }
+            
         }
 
     }
