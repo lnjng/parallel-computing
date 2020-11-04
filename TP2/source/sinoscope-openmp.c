b78c1bd423cd5eaa15ff83485f1ffa6cb6e73d03
Philippe Belias <ossimfrancois@yahoo.fr>
2020-11-04 00:47:43 -0500

    grain dans l'image disparu max fps 21
    Signed-off-by: Philippe Belias <ossimfrancois@yahoo.fr>

--- TP2/source/sinoscope-openmp.c
+++ TP2/source/sinoscope-openmp.c
@@ -29,37 +29,30 @@ int sinoscope_image_openmp(sinoscope_t* sinoscope) {
     pixel_t pixel;
    
     
-    #pragma omp parallel for schedule(static) private(i,j,k,index,px,py,pixel) collapse(2) reduction(+: value) ordered
+    #pragma omp parallel for schedule(static) private(i,j,k,index,px,py,pixel) collapse(2) reduction(+:value)
     for (i = 0; i < sinoscope->width; i++) { 
         for (j = 0; j < sinoscope->height; j++) {
             px    = sinoscope->dx * j - 2 * M_PI;
             py    = sinoscope->dy * i - 2 * M_PI;
-
-            #pragma omp atomic write
             value = 0.0;
             
+
             for (k = 1; k <= sinoscope->taylor; k += 2) {
                 value += sin(px * k * sinoscope->phase1 + sinoscope->time) / k;
                 value += cos(py * k * sinoscope->phase0) / k;
             }
 
-            #pragma omp atomic write
+
             value = (atan(value) - atan(-value)) / M_PI;
-            #pragma omp atomic write
             value = (value + 1) * 100;
-               
+
+            #pragma omp critical(section1)
             color_value(&pixel, value, sinoscope->interval, sinoscope->interval_inverse);
-            
-           
             index = (i * 3) + (j * 3) * sinoscope->width;
-            
-            for (int indice = 0; indice < 3; indice++)
-            {
-                #pragma omp atomic write
-                sinoscope->buffer[index + indice] = pixel.bytes[indice];          
-                
-            }
-            
+
+            sinoscope->buffer[index + 0] = pixel.bytes[0]; 
+            sinoscope->buffer[index + 1] = pixel.bytes[1]; 
+            sinoscope->buffer[index + 2] = pixel.bytes[2];          
             
         }
 
