3637599a180a41871e9bf0ba7395062f6c4c864f
Philippe Belias <ossimfrancois@yahoo.fr>
2020-11-07 17:07:37 -0500

    augmentation du nombre de fps à 40 sans probleme de grain de sable
    Signed-off-by: Philippe Belias <ossimfrancois@yahoo.fr>

--- TP2/source/sinoscope-openmp.c
+++ TP2/source/sinoscope-openmp.c
@@ -23,61 +23,58 @@ int sinoscope_image_openmp(sinoscope_t* sinoscope) {
         goto fail_exit;
     }
 
-    int i;
-    int j;
-    int k;
-    int index;
-    float px;
-    float py;
-    float value = 0.0;
+    int i,j,k, index;
+    float px, py,value = 0.0;
+    pixel_t pixel;
 
+    sinoscope_t sinos = *sinoscope;
+    unsigned char* buffer = sinos.buffer;
     double* values = malloc(sizeof(double) * (sinoscope->buffer_size));
 
     
-    #pragma omp parallel for schedule(static) private(i,j,k,index,px,py) collapse(2) reduction(+:value)
-    for (i = 0; i < sinoscope->width; i++) { 
-        for (j = 0; j < sinoscope->height; j++) {
-            px    = sinoscope->dx * j - 2 * M_PI;
-            py    = sinoscope->dy * i - 2 * M_PI;
+    #pragma omp parallel for schedule(static) private(i,j,k,index,px,py,pixel,value) collapse(2)
+    for (i = 0; i < sinos.width; i++) { 
+        for (j = 0; j < sinos.height; j++) {
+            px    = sinos.dx * j - 2 * M_PI;
+            py    = sinos.dy * i - 2 * M_PI;
             value = 0.0;
             
-
-            for (k = 1; k <= sinoscope->taylor; k += 2) {
-                value += sin(px * k * sinoscope->phase1 + sinoscope->time) / k;
-                value += cos(py * k * sinoscope->phase0) / k;
+            for (k = 1; k <= sinos.taylor; k += 2) {
+                value += sin(px * k * sinos.phase1 + sinos.time) / k + cos(py * k * sinos.phase0) / k;
             }
 
 
             value = (atan(value) - atan(-value)) / M_PI;
             value = (value + 1) * 100;
-            //pixel_t pixel;
 
-            
-            //color_value(&pixel, value, sinoscope->interval, sinoscope->interval_inverse);
-            index = (i * 3) + (j * 3) * sinoscope->width;
+            color_value(&pixel, value, sinos.interval, sinos.interval_inverse);
+            index = (i * 3) + (j * 3) * sinos.width;
             values[index] = value;
-            //pix[index] = pixel;
+            
 
-         
-            //sinoscope->buffer[index + 0] = pixel.bytes[0]; 
-            //sinoscope->buffer[index + 1] = pixel.bytes[1]; 
-            //sinoscope->buffer[index + 2] = pixel.bytes[2];       
+            /*buffer[index + 0] = pixel.bytes[0]; 
+            buffer[index + 1] = pixel.bytes[1]; 
+            buffer[index + 2] = pixel.bytes[2];*/
+            
         }
 
     }
 
-    for (int i = 0; i < sinoscope->width; i++)
+    //#pragma omp parallel for schedule(static) private(i,j,index) firstprivate(buffer) collapse(2) ordered
+    for (i = 0; i < sinos.width; i++)
     {
-        for (int j = 0;  j < sinoscope->height; j++)
-        {
-            int index = (i * 3) + (j * 3) * sinoscope->width;
-            double value = values[index];
-            pixel_t pixel;
-            color_value(&pixel, value, sinoscope->interval, sinoscope->interval_inverse);
-            sinoscope->buffer[index + 0] = pixel.bytes[0]; 
-            sinoscope->buffer[index + 1] = pixel.bytes[1]; 
-            sinoscope->buffer[index + 2] = pixel.bytes[2];
 
+        for (j = 0;  j < sinos.height; j++)
+        {
+            index = (i * 3) + (j * 3) * sinos.width;
+            pixel_t pixel;
+            color_value(&pixel, values[index], sinos.interval, sinos.interval_inverse);
+            //#pragma omp ordered
+            //{
+                buffer[index + 0] = pixel.bytes[0]; 
+                buffer[index + 1] = pixel.bytes[1]; 
+                buffer[index + 2] = pixel.bytes[2];   
+            //}
         }
     }
 
